e83e25446dd39a29df43fc37cb857bdad6fbdb89
ASUS <ASUS@LAPTOP-1VVELRKS>
2018-09-12 12:53:34 +0700

    Initial Commit

---
+++ src/HitungFibonacci.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class HitungFibonacci {
+    private static void tampilJudul (String identitas){
+        System.out.println("Identitas : "+identitas);
+        
+        System.out.println("/nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst. \n");
+    }
+        public static void main (String [] args){
+        String identitas = "KARINA SHAFA AMALIA / XRL5/ 17";
+        tampilJudul(identitas);
+        
+    }
+}
