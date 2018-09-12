8dd2f8b046141eca63717d0283731af4ec694607
ASUS <ASUS@LAPTOP-1VVELRKS>
2018-09-12 13:14:24 +0700

    Tambah Tampil Hasil

--- src/HitungFibonacci.java
+++ src/HitungFibonacci.java
@@ -23,6 +23,8 @@ public class HitungFibonacci {
         String identitas = "KARINA SHAFA AMALIA / XRL5/ 17";
         tampilJudul(identitas);
         int n = tampilInput();
+        BigInteger hasil = fibo(n);
+        tampilHasil(n, hasil);
         }
         private static int tampilInput(){
             Scanner scanner = new Scanner(System.in);
@@ -45,5 +47,8 @@ public class HitungFibonacci {
         }
         
         return hasil [n-1];
+    }
+    private static void tampilHasil (int n, BigInteger hasil){
+        System.out.println("Bilangan Fibonacci ke-"+n+" : " +hasil);
     }
     }
