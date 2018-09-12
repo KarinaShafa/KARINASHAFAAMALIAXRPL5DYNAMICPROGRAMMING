ed6a8c91ff05abf2d500aa346dde0e243ea82535
ASUS <ASUS@LAPTOP-1VVELRKS>
2018-09-12 13:01:11 +0700

    Tambah Input

--- src/HitungFibonacci.java
+++ src/HitungFibonacci.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -18,6 +21,16 @@ public class HitungFibonacci {
         public static void main (String [] args){
         String identitas = "KARINA SHAFA AMALIA / XRL5/ 17";
         tampilJudul(identitas);
+        int n = tampilInput();
+        }
+        private static int tampilInput(){
+            Scanner scanner = new Scanner(System.in);
         
-    }
-}
+            System.out.print("Bilangan ke-: ");
+            int n = scanner.nextInt();
+            
+            return n;
+        }
+        
+        }   
+    
